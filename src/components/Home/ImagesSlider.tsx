ac12d01e47caf5b44b48d53f288c70ffdc923c32
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2025-03-08 00:11:16 +0530

    Update ImagesSlider.tsx

--- src/components/Home/ImagesSlider.tsx
+++ src/components/Home/ImagesSlider.tsx
@@ -33,7 +33,7 @@ export function HeroImagesSlider() {
           Elevate your wardrobe with timeless pieces that define luxury.
         </motion.p>
         <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <span>Join now →</span>
+          <span>Shop Now →</span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </button>
       </motion.div>
