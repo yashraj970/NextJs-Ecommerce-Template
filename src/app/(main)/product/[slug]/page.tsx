211b320a0b331586c76ecdc56622c202ff4810d8
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2025-03-12 18:49:23 +0530

    Update page.tsx

--- src/app/(main)/product/[slug]/page.tsx
+++ src/app/(main)/product/[slug]/page.tsx
@@ -106,12 +106,12 @@ export async function generateMetadata({
     },
   };
 }
-
-export default async function ProductPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+interface ProductPageProps {
+  params: {
+    slug: string;
+  };
+}
+export default async function ProductPage({ params }: ProductPageProps) {
   const product = await getProduct(params.slug);
 
   if (!product) {
